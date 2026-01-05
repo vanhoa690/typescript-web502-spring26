2265e65f75a7ddc813fb5eed216df5b70277c81f
vanhoa690 <vanhoa690@gmail.com>
2026-01-05 19:25:55 +0700

    create new typescript

---
+++ learn-typescript/script.js
@@ -0,0 +1,5 @@
+console.log("javascript");
+console.log("hoadv");
+
+// let myName: string = "hoadv";
+// console.log(myName);
---
+++ learn-typescript/typescript.ts
@@ -0,0 +1,3 @@
+console.log("typescript");
+let myName: string = "hoadv";
+console.log(myName);
