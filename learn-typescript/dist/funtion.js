6307e4d50ba2281d08c0215295dbe935fc64dc9a
vanhoa690 <vanhoa690@gmail.com>
2026-01-12 19:16:27 +0700

    class interface type

--- learn-typescript/dist/funtion.js
+++ learn-typescript/dist/funtion.js
@@ -14,9 +14,31 @@ function logMessage(message) {
 }
 // props trong react sang  component : onClick: () => void
 // props trong react sang  component : onSearch: (keyword: string) => string
-// Tham số mặc định (Default Parameter)
+// 3. Tham số mặc định (Default Parameter)
 function greetUser(name, greeting = "hello") {
     return `${greeting}, ${name}!`;
 }
 greetUser("Alice"); // "hello, Alice!"
 greetUser("Alice", "Xin Chao"); // "Xin Chao, Alice!"
+// 4. Tham số tùy chọn (Optional Parameter)
+function describePerson(name, age) {
+    if (age) {
+        return `${name} is ${age} years old.`;
+    }
+    return `${name} has no age specified.`;
+}
+// 5. Toán tử rest (rest parameter)
+function sum(...numbers) {
+    return numbers.reduce((total, num) => total + num, 0);
+}
+sum(1, 2);
+sum(1, 2, 3, 4, 5);
+// 6. Spread operator
+const arr1 = [1, 2, 3];
+const arr2 = [4, 5];
+const combinedArray = [...arr1, ...arr2];
+console.log(combinedArray);
+const arr3 = ["a", "b", "c"];
+const arr4 = [1, 2, 3];
+const result = [...arr3, ...arr4];
+console.log(result);
